4a4fa657b88fd9ee119cbb168f07b26126d2111c
cagataycali <cagataycali@icloud.com>
2017-10-02 23:36:53 +0300

    📦  Refactored and formatted

--- Paralelkenar.c
+++ Paralelkenar.c
@@ -1,37 +1,30 @@
-#include<stdio.h>
-int main()
-{
-	int i;
-	int z;
-	int j;
-	int a,b;
-	int bosluk=1;
-	printf("Uzun kenarda kac yildiz olmasini istersiniz:");
-	scanf("%d",&a);
+#include <stdio.h>
 
-	printf("Kisa kenarda kac yildiz olmasiniz istersiniz:");
-	scanf("%d",&b);
+  int main() {
+    int i;
+    int z;
+    int j;
+    int a, b;
+    int bosluk = 1;
+    printf('Uzun kenarda kaç yıldız olsun istersin:');
+    scanf("%d", &a);
 
-	for(i = 0; i < b ; i++)
-	{
-		for(j = 0; j < a ; j++)
-		{
-		printf("*");
-		}
+    printf('Kısa kenarda kac yıldız olmasını istersin:');
+    scanf("%d", &b);
 
-	printf("\n");
+    for (i = 0; i < b; i++) {
+      for (j = 0; j < a; j++) {
+        printf("*");
+      }
 
-		for(z = 0; z < bosluk ; z++)
-		{
-		printf(" ");
-		if(bosluk = b)
-		break;
-		}
+      printf("\n");
 
-	bosluk = bosluk + 1;
-
-	}
-
-	return 0;
-
-}
+      for (z = 0; z < bosluk; z++) {
+        printf(" ");
+        if (bosluk = b)
+          break;
+      }
+      bosluk = bosluk + 1;
+    }
+    return 0;
+  }
